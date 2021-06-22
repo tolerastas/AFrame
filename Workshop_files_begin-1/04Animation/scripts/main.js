f60a44576f1d607e03c1eff01d0ea8ee3aa5f2ac
tolerastas <34009778+tolerastas@users.noreply.github.com>
2021-06-22 22:14:46 +0200

    Update main.js

--- Workshop_files_begin-1/04Animation/scripts/main.js
+++ Workshop_files_begin-1/04Animation/scripts/main.js
@@ -5,7 +5,7 @@
  var myOtherBox = document.getElementById('myOtherBox');
 
  function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed;
+	myOtherBox.object3D.rotation.x -= rotationSpeed;
  	console.log(myOtherBox.object3D.rotation.x);
  }
 
