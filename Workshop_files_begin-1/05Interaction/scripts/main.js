e9f31f7b172877749fe1bb0132525188de0c06e3
tolerastas <34009778+tolerastas@users.noreply.github.com>
2021-10-23 00:02:50 +0200

    Update main.js

--- Workshop_files_begin-1/05Interaction/scripts/main.js
+++ Workshop_files_begin-1/05Interaction/scripts/main.js
@@ -16,3 +16,8 @@ myOtherBox.addEventListener('mouseenter', function(){
  	rotationSpeed = 0.01;
  	console.log('mouse enter');
 });
+
+myOtherBox.addEventListener('mouseleave', function(){
+ 	rotationSpeed = 0.001;
+ 	console.log('mouse leave');
+});
