35119767326e5bc4f3b022499653df86671fd29e
tolerastas <34009778+tolerastas@users.noreply.github.com>
2021-10-23 00:13:23 +0200

    Update main.js

--- Workshop_files_begin-1/05Interaction/scripts/main.js
+++ Workshop_files_begin-1/05Interaction/scripts/main.js
@@ -1,16 +1,29 @@
 
 var rotationSpeed = 0.001;
 var myOtherBox = document.getElementById('myOtherBox');
+var myBox = document.getElementById('myBox');
 
 function spin(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
+	myBox.object3D.rotation.x += rotationSpeed;
 	myOtherBox.object3D.rotation.y += rotationSpeed;
 	myOtherBox.object3D.rotation.z += rotationSpeed/2;
 	console.log(myOtherBox.object3D.rotation);
+	console.log(myBox.object3D.rotation);
 }
 
 setInterval(spin, 16);
 
+
+myBox.addEventListener('mouseenter', function(){
+ 	rotationSpeed = 0.01;
+ 	console.log('mouse enter');
+});
+
+myBox.addEventListener('mouseleave', function(){
+ 	rotationSpeed = 0.001;
+ 	console.log('mouse leave');
+});
 
 myOtherBox.addEventListener('mouseenter', function(){
  	rotationSpeed = 0.01;
