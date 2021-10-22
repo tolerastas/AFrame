be42a979dc9ba4c644a9f32a36c70aa9df9253fe
tolerastas <34009778+tolerastas@users.noreply.github.com>
2021-10-22 23:57:29 +0200

    Update main.js

--- Workshop_files_begin-1/05Interaction/scripts/main.js
+++ Workshop_files_begin-1/05Interaction/scripts/main.js
@@ -6,7 +6,7 @@ function spin(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
 	myOtherBox.object3D.rotation.y += rotationSpeed;
 	myOtherBox.object3D.rotation.z += rotationSpeed/2;
-	//console.log(myOtherBox.object3D.rotation);
+	console.log(myOtherBox.object3D.rotation);
 }
 
 setInterval(spin, 16);
